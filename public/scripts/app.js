73f691cff169e14e913fd6196bfce4dc0eef99ac
Thaveesha98 <thaveeshayanith98@gmail.com>
2025-01-03 12:06:11 +0530

    add:Loading behaviour while  get weather

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-console.log("Client side javascript is available");
+console.log("Client-side JavaScript is available");
 const endPoint = "weather";
 const address = document.querySelector("input");
 const area = document.querySelector("#location");
@@ -6,31 +6,38 @@ const weatherCondition = document.querySelector("#weatherCondition");
 const temp = document.querySelector("#temp");
 const feelslike = document.querySelector("#feelslike");
 const humidity = document.querySelector("#humidity");
-
 const error = document.querySelector("#error");
 
 const weatherFrom = document.querySelector("form");
 const weather = (address) => {
+  // Display loading text
+  area.textContent = "Loading...";
+
   fetch(`${endPoint}?location=${address}`).then((res) => {
-    res.json().then((data) => {
-      if (data.error) {
-        (error.textContent = "Error :"), data.error;
-      } else {
-        console.log(data);
-        area.textContent = `Address ${data.location}`;
-        weatherCondition.textContent = `The weather Condition is ${data.weatherCondition}`;
-        temp.textContent = `Actual Temperature is ${data.temp}`;
-        feelslike.textContent = `But Temperature feels like ${data.feelslike}`;
-        humidity.textContent = `The humidity is ${data.humidity}`;
-      }
-    });
+    res
+      .json()
+      .then((data) => {
+        if (data.error) {
+          error.textContent = `Error: ${data.error}`;
+        } else {
+          console.log(data);
+          area.textContent = `Address: ${data.location}`;
+          weatherCondition.textContent = `The weather Condition is ${data.weatherCondition}`;
+          temp.textContent = `Actual Temperature is ${data.temp}`;
+          feelslike.textContent = `But Temperature feels like ${data.feelslike}`;
+          humidity.textContent = `The humidity is ${data.humidity}`;
+        }
+      })
+      .catch(() => {
+        error.textContent = "Failed to fetch data.";
+      });
   });
 };
 
 weatherFrom.addEventListener("submit", (e) => {
   e.preventDefault();
   if (!address.value) {
-    error.textContent = "you must provide address";
+    error.textContent = "You must provide an address";
   } else {
     weather(address.value);
   }
